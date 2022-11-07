3b01eec3a1eb05de8f21342e13e47e1f701d214b
jimchen5209 <jimchen5209@gmail.com>
2022-11-07 10:36:23 +0800

    fix: prevent loading state when data present

--- src/pages/Lock.js
+++ src/pages/Lock.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import { lockStatus, lockStatusNoLoading } from "../redux/lockSlice";
-import { useDispatch } from "react-redux";
+import { lockStatus, lockStatusNoLoading, selectLock } from "../redux/lockSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./Lock.css";
 import LockContent from "../components/LockContent";
@@ -13,12 +13,13 @@ import RefreshIcon from "@mui/icons-material/Refresh";
 const Luck = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const { lockList } = useSelector(selectLock);
   const handleClickRefresh = () => {
     dispatch(lockStatus());
   };
 
   useEffect(() => {
-    dispatch(lockStatus());
+    if (lockList.length === 0) dispatch(lockStatus());
     let refresh = setInterval(() => {
       dispatch(lockStatusNoLoading());
     }, 3000);
