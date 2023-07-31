9b22f0ca707337c39ab4557f968eeb7622b29d4e
peggy0418 <but5863841@gmail.com>
2023-07-31 11:56:08 +0800

    改回原本介面+加上開鎖介面選項按鈕

--- src/components/Readmode.js
+++ src/components/Readmode.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useRef } from "react";
-import ExpandMore from '@mui/icons-material/ExpandMore'
+import React, { useEffect } from "react";
 import {
-  Accordion,
   Collapse,
   TextField,
   Alert,
   Stack,
   Skeleton,
   Button,
-  Typography,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
-  AccordionSummary,
 } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
@@ -27,36 +23,30 @@ import { useSelector, useDispatch } from "react-redux";
 import { lockStatus } from "../redux/lockSlice";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+
 import "./Readmode.css";
 
 const Readmode = (props) => {
   const { t } = useTranslation();
 
   const [open, setOpen] = React.useState(false);
-  const [submittedReason, setSubmittedReason] = React.useState(''); // 提交後的原因
-  const [isSubmitted, setIsSubmitted] = React.useState(false); // 是否已經提交
-  const [locked, setLocked] = React.useState(true);
-  // const [alertValue, setAlertValue] = React.useState({
-  //   show: false,
-  //   type: "success",
-  //   text: "",
-  // });
+  const [alertValue, setAlertValue] = React.useState({
+    show: false,
+    type: "success",
+    text: "",
+  });
   const [checkOpen, setCheckOpen] = React.useState(false);
   const [inputDescription, setInputDescription] = React.useState("");
-  // const dispatch = useDispatch();
-  // const location = useLocation();
-  // const [update, setUpdate] = React.useState(false);
-  const [expandedAccordionIndex, setExpandedAccordionIndex] = React.useState(-1);
-  const accordionRef = useRef(null); // 步驟2：新增 useRef
-
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [update, setUpdate] = React.useState(false);
 
   const { user, isFetching, isUnlocking } = useSelector(selectUser);
 
   const handleEdit = () => {
     props.setUserStatus("EditStatus");
   };
-
-  const inputRef = useRef(null);
+  const inputRef = React.useRef(null);
 
   const handleButtonClick = (event) => {
     const buttonText = event.target.innerText;
@@ -66,7 +56,6 @@ const Readmode = (props) => {
       inputRef.current.focus();
     }
   };
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -83,89 +72,46 @@ const Readmode = (props) => {
     }
   };
 
-  const handleCheckCloseAPI = () => {
+  const handleCheckCloseAPI = async () => {
     setCheckOpen(false);
-    setSubmittedReason(inputDescription); // 提交後將原因保存
-    setIsSubmitted(true); // 將 isSubmitted 設為 true
-    setCheckOpen(false); // 提交後關閉對話框
-    setInputDescription("")
-    setLocked(!locked);
-    alert("已解鎖");
-    setSubmitCount(submitCount + 1);
-
-
-
-
-    // const a = await dispatch(
-    //   userUnlock([{ lockerNo: location.state, description: inputDescription }])
-    // );
-    // setUpdate(true);
-    // dispatch(lockStatus());
-    // if (a.payload === 0) {
-    //   setAlertValue({
-    //     type: "success",
-    //     text: t("finishForced"),
-    //     show: true,
-    //   });
-    // } else {
-    //   setAlertValue({
-    //     type: "error",
-    //     text: t("failedForced"),
-    //     show: true,
-    //   });
-    // }
+    const a = await dispatch(
+      userUnlock([{ lockerNo: location.state, description: inputDescription }])
+    );
+    setUpdate(true);
+    dispatch(lockStatus());
+    if (a.payload === 0) {
+      setAlertValue({
+        type: "success",
+        text: t("finishForced"),
+        show: true,
+      });
+    } else {
+      setAlertValue({
+        type: "error",
+        text: t("failedForced"),
+        show: true,
+      });
+    }
   };
-
-    // 操作時間
-    const [currentTime, setCurrentTime] = React.useState(new Date()); 
-    const [submitCount, setSubmitCount] = React.useState(0);
-  
-
-    useEffect(() => {
-      // 只有在提交次數改變時才更新時間
-      setCurrentTime(new Date());
-    }, [submitCount]);
-  
-    const formattedTime = currentTime.toLocaleTimeString();
-
-
-
 
   useEffect(() => {
-    if (expandedAccordionIndex !== -1 && accordionRef.current) {
-      accordionRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    if (update) {
+      dispatch(userInfo(location.state));
+      setInputDescription("");
+      setUpdate(false);
     }
-  }, [expandedAccordionIndex]);
+  }, [update, dispatch, location]);
 
-  const handleClickAccordion = (index) => {
-    if (index === expandedAccordionIndex) {
-      // 如果手風琴已經展開，則關閉它
-      setExpandedAccordionIndex(-1);
-
-    } else {
-      // 如果手風琴未展開，則展開它
-      setExpandedAccordionIndex(index);
+  useEffect(() => {
+    if (alertValue.show) {
+      setTimeout(() => {
+        setAlertValue({
+          ...alertValue,
+          show: false,
+        });
+      }, 3000);
     }
-  };
-
-  // useEffect(() => {
-  //   if (update) {
-  //     dispatch(userInfo(location.state));
-  //     setInputDescription("");
-  //     setUpdate(false);
-  //   }
-  // }, [update, dispatch, location]);
-
-  // useEffect(() => {
-  //   if (alertValue.show) {
-  //     setTimeout(() => {
-  //       setAlertValue({
-  //         ...alertValue,
-  //         show: false,
-  //       });
-  //     }, 3000);
-  //   }
-  // }, [alertValue]);
+  }, [alertValue]);
 
   const handleCheckClose = () => {
     setInputDescription("");
@@ -211,40 +157,36 @@ const Readmode = (props) => {
           onClick={handleEdit}
           variant="contained"
           style={{
-            width: "40%",
+            width: "100%",
             height: 39,
             background: "#363f4e",
             boxShadow: "none",
             fontSize: 18,
-            // marginTop: 10,
-            marginRight: 10,
+            margin: 5,
           }}
           startIcon={<EditIcon />}
         >
           {t("edit")}
         </Button>
 
-        {/* 強制開鎖按鈕 */}
         <LoadingButton
           loading={isUnlocking}
           disabled={isUnlocking}
           variant="contained"
           onClick={handleClickOpen}
           style={{
-            width: "40%",
+            width: "100%",
             height: 39,
             background: "#FFC440",
             boxShadow: "none",
             fontSize: 18,
-            // marginTop: 10,
-            marginRight: 10,
+            margin: 5,
           }}
           startIcon={<LockOpenIcon />}
         >
           {t("forced")}
         </LoadingButton>
 
-        {/* 強制開鎖原因介面 */}
         <Dialog
           open={open}
           onClose={handleClose}
@@ -257,7 +199,7 @@ const Readmode = (props) => {
             "& .MuiDialog-container": {
               "& .MuiPaper-root": {
                 width: 440,
-                height: 400, // Set your width here
+                height: 300, // Set your width here
               },
               "& .MuiOutlinedInput-root": {
                 width: 328,
@@ -278,25 +220,22 @@ const Readmode = (props) => {
                 required
                 multiline
                 value={inputDescription}
-                onChange={(event) => setInputDescription(event.target.value)}
+                onChange={(e) => setInputDescription(e.target.value)}
                 id="input-reason"
                 placeholder={t("reminderContent")}
-                inputRef={inputRef}
                 sx={{
                   "& .MuiOutlinedInput-root": {
                     display: "flex",
                     alignItems: "flex-start",
                     "&.Mui-focused fieldset": {
-                      borderColor: "gray",
+                      borderColor: "gray", //FIELD 框
                     },
                   },
                 }}
               />
             </DialogContent>
           </div>
-          <Typography variant="h6" sx={{ textAlign: "center" }} >{t("cause_click")}</Typography>
-          {/* 選項button */}
-          <DialogActions sx={{ justifyContent: "center" }}>
+           <DialogActions sx={{ justifyContent: "center" }}>
             <Button
               onClick={handleButtonClick}
               variant="contained"
@@ -329,7 +268,8 @@ const Readmode = (props) => {
                 margin: 5,
               }}>{t("forgot card")}</Button>
           </DialogActions>
-          <DialogActions sx={{ justifyContent: "center" }}>
+
+          <DialogActions sx={{ width: 328 }}>
             <Button
               variant="contained"
               onClick={handleClickCheckOpen}
@@ -375,11 +315,11 @@ const Readmode = (props) => {
             "& .MuiDialog-container": {
               "& .MuiPaper-root": {
                 width: 375,
-                height: 250, // Set your width here
+                height: 250, 
                 borderRadius: "10px",
               },
               "& .MuiOutlinedInput-root": {
-                width: 244, // Set your width here
+                width: 244, 
                 height: 150,
               },
               "& .MuiDialogContent-root ": {
@@ -434,7 +374,7 @@ const Readmode = (props) => {
           </DialogActions>
         </Dialog>
       </div>
-      {/* <Stack
+      <Stack
         className="success"
         sx={{
           width: "478px",
@@ -450,38 +390,8 @@ const Readmode = (props) => {
             {alertValue.text}
           </Alert>
         </Collapse>
-      </Stack> */}
-
-      {/* 將提交後的原因渲染到頁面上 */}
-
-      {isSubmitted && (
-        <Accordion
-          id="accordion"
-          ref={accordionRef} // 步驟4：將 ref 綁定到手風琴
-          expanded={expandedAccordionIndex === 0} // 當索引為 0 時展開第一個手風琴
-          onChange={() => handleClickAccordion(0)}  // 點擊時切換展開狀態
-          sx={{
-            width: "1000px", marginTop: "100px", backgroundColor: "rgb(112, 112, 112)"
-          }}>
-
-          <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel-content" id="panel-header">
-            <Typography sx={{ fontSize: "18px" }}>{t("unlocked cause")}</Typography>
-          </AccordionSummary>
-          <Collapse in={expandedAccordionIndex === 0}>
-            <Typography sx={{ p: 2 }}>
-              root: {submittedReason} 
-            </Typography>
-            <span>&nbsp;&nbsp;&nbsp;操作時間:{formattedTime}</span>
-          </Collapse>
-        </Accordion>
-
-
-      )}
-
-
-
+      </Stack>
     </div>
-
   );
 };
 
